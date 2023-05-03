34e6bf01124c801269eefe2457be6de8fa4243d8
foisal404 <foisalahmmed2@gmail.com>
2023-05-03 23:33:42 +0600

    recipies json file added and send using req

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ app.use(cors())
 
 const port=5000
 
+const recipies=require('./data/recipies.json')
+
 app.get('/',(req,res)=>[
     res.send('Chef is running')
 ])
+
+app.get('/recipies',(req,res)=>{
+    res.send(recipies)
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
