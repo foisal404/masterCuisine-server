994ba251333288738a3b7eea649bb197d9b520a0
foisal404 <foisalahmmed2@gmail.com>
2023-05-04 00:26:56 +0600

    chefs link dynamic chef link add

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express=require('express')
-
 var cors = require('cors')
 var app = express()
-
 app.use(cors())
-
 const port=5000
 
 const recipies=require('./data/recipies.json')
+const chefs=require('./data/chefs.json')
 
 app.get('/',(req,res)=>[
     res.send('Chef is running')
 ])
 
+//recipies
 app.get('/recipies',(req,res)=>{
     res.send(recipies)
 })
@@ -20,6 +19,17 @@ app.get("/recipies/:id",(req,res)=>{
     const id=req.params.id;
     const recipie=recipies.find(reci=>reci._id===id)
     res.send(recipie)
+})
+
+// chefs 
+app.get('/chefs',(req,res)=>{
+    res.send(chefs)
+})
+
+app.get('/chefs/:id',(req,res)=>{
+    const id=req.params.id;
+    const chef=chefs.find(che=>che._id===id)
+    res.send(chef)
 })
 
 app.listen(port, () => {
